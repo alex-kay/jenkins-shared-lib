49bac53d345c83ab89261e99ce88c62412a9f3f8
alex-kay <ichijoint@gmail.com>
2021-10-06 04:42:58 +0300

    fixed adding build tag to commit

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -1,13 +1,7 @@
 #!/usr/bin/env groovy
 def call() {
   withCredentials([usernamePassword(credentialsId: 'github_key', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GITHUB_ACCESS_TOKEN')]) {
-    sh('git tag -a "${BUILD_NUMBER}" -m "Added build tag"')
+    sh('git tag -a "build-${BUILD_NUMBER}" -m "Added build tag"')
     sh('git push https://${GITHUB_ACCESS_TOKEN}@github.com/alex-kay/jenkins-homework.git --tags')
 }
-            sh '''
-            npm run build
-            git tag build-${BUILD_NUMBER}
-            git push --tags
-            '''
 }
-
