d538a5b44fffd471e4a69ba9301bf5a2f832ffd1
alex-kay <ichijoint@gmail.com>
2021-10-06 04:39:29 +0300

    -

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 def call() {
-  withCredentials([usernamePassword(credentialsId: 'github_key', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+  withCredentials([usernamePassword(credentialsId: 'github_key', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GITHUB_ACCESS_TOKEN')]) {
     sh('git tag -a "${BUILD_NUMBER}" -m "Added build tag"')
-    sh('git push https://${github_key}@github.com/alex-kay/jenkins-homework.git --tags')
+    sh('git push https://${GITHUB_ACCESS_TOKEN}@github.com/alex-kay/jenkins-homework.git --tags')
 }
             sh '''
             npm run build
