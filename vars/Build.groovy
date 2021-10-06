242af9d526b117c2a6a61a1914750679b155dde2
alex-kay <ichijoint@gmail.com>
2021-10-06 04:23:52 +0300

    test

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -2,7 +2,7 @@
 def call() {
   withCredentials([usernamePassword(credentialsId: 'github_key', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
     sh('git tag -a "${BUILD_NUMBER}" -m "Added build tag"')
-    sh('git push --tags')
+    sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@ --tags')
 }
             sh '''
             npm run build
